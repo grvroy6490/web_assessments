f2ec719c482a3855b8c33d6fef788ef4591a4837
grvroy6490 <roy.gourav6490@gmail.com>
2024-05-23 13:52:01 +0530

    first commit

---
+++ Q1/app.js
@@ -0,0 +1,11 @@
+/* 
+Write a JavaScript function using ES6 syntax (without using any in-built functions) 
+that takes an array of numbers and returns a new array with only the even numbers squared. 
+*/
+
+//Example:
+squareEvenNumbers([1, 2, 3, 4, 5, 6]); // Output should be [4, 16, 36]
+ 
+
+
+// START CODING FROM HERE
---
+++ Q2/app.js
@@ -0,0 +1,16 @@
+/* 
+
+You are tasked with building a simple web application that fetches user data from an API when a button is clicked. 
+Upon receiving the data, the application should display the user's name and email address on the webpage. 
+The API endpoint for fetching user data is “https://jsonplaceholder.typicode.com/users/{userId}” where {userId} is the ID of the user to fetch.
+
+Write the JavaScript code to implement this functionality. Ensure that:
+
+When the button is clicked, the application fetches user data asynchronously from the API.
+The user's name and email address are displayed on the web page once the data is fetched.
+Handle any errors that may occur during the fetching process, such as network errors or invalid responses from the API. 
+
+*/
+
+
+// START CODING FROM HERE
